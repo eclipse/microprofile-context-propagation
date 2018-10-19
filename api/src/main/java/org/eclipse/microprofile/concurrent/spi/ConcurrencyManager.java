365fd9e051bdacc6053a09a87b1d121c0542d528
Stephane Epardaud <stef@epardaud.fr>
2018-10-19 15:19:39 +0200

    Support per-classloader configuration and specify when to load the ThreadContextProviders

---
+++ api/src/main/java/org/eclipse/microprofile/concurrent/spi/ConcurrencyManager.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eclipse.microprofile.concurrent.spi;
+
+import org.eclipse.microprofile.concurrent.ManagedExecutorBuilder;
+import org.eclipse.microprofile.concurrent.ThreadContextBuilder;
+
+/**
+ * {@link ConcurrencyManager} instances can be used to create {@link #newManagedExecutorBuilder()}
+ * or {@link #newThreadContextBuilder()}. Each {@link ConcurrencyManager} instance has its own set
+ * of {@link ThreadContextProvider} as defined during building with {@link ConcurrencyManagerBuilder#build()}
+ * or {@link ConcurrencyProvider#getConcurrencyManager()}.
+ *
+ * @see ConcurrencyProvider#getConcurrencyManager()
+ * @see ConcurrencyManagerBuilder
+ */
+public interface ConcurrencyManager {
+    /**
+     * Creates a new <code>ManagedExecutorBuilder</code> instance.
+     *
+     * @return a new <code>ManagedExecutorBuilder</code> instance.
+     */
+    ManagedExecutorBuilder newManagedExecutorBuilder();
+
+    /**
+     * Creates a new <code>ThreadContextBuilder</code> instance.
+     *
+     * @return a new <code>ThreadContextBuilder</code> instance.
+     */
+    ThreadContextBuilder newThreadContextBuilder(); 
+
+}
